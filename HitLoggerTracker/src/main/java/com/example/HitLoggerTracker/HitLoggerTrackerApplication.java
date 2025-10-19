2975e47582efe6ab7102766b55e8f5764dd2c52e
leunbin <silverbin2105@gmail.com>
2025-10-19 18:21:31 +0900

    feat.hit logger tracker

---
+++ HitLoggerTracker/src/main/java/com/example/HitLoggerTracker/HitLoggerTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HitLoggerTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitLoggerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitLoggerTrackerApplication.class, args);
+	}
+
+}
---
+++ HitLoggerTracker/src/test/java/com/example/HitLoggerTracker/HitLoggerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HitLoggerTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitLoggerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
